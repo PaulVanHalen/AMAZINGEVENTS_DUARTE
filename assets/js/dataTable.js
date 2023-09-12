ac8197dc70f9212b8fbca85e59a8e64889f107f9
paulDev <paul_d18@hotmail.com>
2023-09-12 17:05:17 -0300

    update

--- assets/js/dataTable.js
+++ assets/js/dataTable.js
@@ -1,16 +1,14 @@
 let rootApi = "https://mindhub-xj03.onrender.com/api/amazing";
+let currentDate ;
 
-async function getStats(){
+async function getStats(callback){
 try {
     let respuesta = await fetch(rootApi);
-    let statsApi = await respuesta.json();
-    console.log(statsApi);
+    let data = await respuesta.json();
+    console.log(data);
+    currentDate = data.currentDate;
+    callback(data.events, currentDate);
 } catch (error) {
-    console.log(error);
 }
-
 }
-
-getStats();
-
 
--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,136 +1,29 @@
-// creando la tabla
 
-let container = document.getElementById("#tableBody");
+let container = document.getElementById("tableBody");
+let container1 = document.getElementById("tableBody");
+let container2 = document.getElementById("tableBody");
 
 getStats(createTable);
 
-//filtro por nombre a los eventos, asi accedo mas facil a ellos
-
-function createTable(events){
+function createTable(events, currentDate){
     let tbodyHTML = "";
-    events.forEach(events =>{
-        let eventsFiltereds = events.filter(events =>{
-        let eventName = events.name.map(event => event.name);
-        if (eventName.includes(events))
-        {return true;}
-        else {
-            return false;
-        }
-        
-    });
-        let eventName = events.name;
-        let mayorAsistencia = getMayorAsistencia(eventsFiltereds);
-        let menorAsistencia = getMenorAsistencia(eventsFiltereds);
-        let mayorCapacidad = getMayorCapacidad(eventsFiltereds);
+        const eventosPasados = events.filter (evento => evento.date < currentDate)
+        console.log(events);
+        const eventosFuturos = events.filter (evento => evento.date > currentDate)
+
+        let mayorAsistencia = eventosPasados.sort((a, b) => (b.assistance * 100) / b.capacity - (a.assistance * 100) / a.capacity).slice(0, 3);
+        let menorAsistencia = eventosPasados.sort((a, b) => (a.assistance * 100) / a.capacity - (b.assistance * 100) / b.capacity).slice(0, 3);
+        let mayorCapacidad = events.sort((a, b) => b.capacity - a.capacity).slice(0, 3);
+
+        for (let i = 0; i < mayorCapacidad.length; i++){
 
         tbodyHTML += `<tr>
-        <td>${eventName}</td>
-        <td>${mayorAsistencia.name} (${mayorAsistencia.assistance})</td>
-        <td>${menorAsistencia.name} (${menorAsistencia.assistance})</td>
-        <td>${mayorCapacidad.name} (${mayorCapacidad.capacity})</td>
+        <td>${mayorAsistencia[i].name}</td>
+        <td>${menorAsistencia[i].name}</td>
+        <td>${mayorCapacidad[i].name}</td>
         </tr>
         `
-    })
+    }
 
     container.innerHTML = tbodyHTML;
-}
-
-// Todos los eventos
-function getMayorAsistenciaTodos(eventsFiltereds) {
-    return eventsFiltereds.reduce((mayor, actual) => {
-        if (mayor.assistance > actual.assistance) {
-            return mayor;
-        } else {
-            return actual;
-        }
-    });
-}
-
-function getMenorAsistenciaTodos(eventsFiltereds) {
-    return eventsFiltereds.reduce((menor, actual) => {
-        if (menor.assistance < actual.assistance) {
-            return menor;
-        } else {
-            return actual;
-        }
-    });
-}
-
-function getMayorCapacidadTodos(eventsFiltereds) {
-    return eventsFiltereds.reduce((mayor, actual) => {
-        if (mayor.capacity > actual.capacity) {
-            return mayor;
-        } else {
-            return actual;
-        }
-    });
-}
-
-// Eventos pasados
-function getMayorAsistenciaPasados(eventsFiltereds, currentDate) {
-    return eventsFiltereds.reduce((mayor, actual) => {
-        const eventDate = new Date(actual.date);
-        if (eventDate < new Date(currentDate) && mayor.assistance > actual.assistance) {
-            return mayor;
-        } else {
-            return actual;
-        }
-    });
-}
-
-function getMenorAsistenciaPasados(eventsFiltereds, currentDate) {
-    return eventsFiltereds.reduce((menor, actual) => {
-        const eventDate = new Date(actual.date);
-        if (eventDate < new Date(currentDate) && menor.assistance < actual.assistance) {
-            return menor;
-        } else {
-            return actual;
-        }
-    });
-}
-
-function getMayorCapacidadPasados(eventsFiltereds, currentDate) {
-    return eventsFiltereds.reduce((mayor, actual) => {
-        const eventDate = new Date(actual.date);
-        if (eventDate < new Date(currentDate) && mayor.capacity > actual.capacity) {
-            return mayor;
-        } else {
-            return actual;
-        }
-    });
-}
-
-// Eventos futuros
-
-function getMayorAsistenciaFuturos(eventsFiltereds, currentDate) {
-    return eventsFiltereds.reduce((mayor, actual) => {
-        const eventDate = new Date(actual.date);
-        if (eventDate > new Date(currentDate) && mayor.assistance > actual.assistance) {
-            return mayor;
-        } else {
-            return actual;
-        }
-    });
-}
-
-function getMenorAsistenciaFuturos(eventsFiltereds, currentDate) {
-    return eventsFiltereds.reduce((menor, actual) => {
-        const eventDate = new Date(actual.date);
-        if (eventDate > new Date(currentDate) && menor.assistance < actual.assistance) {
-            return menor;
-        } else {
-            return actual;
-        }
-    });
-}
-
-function getMayorCapacidadFuturos(eventsFiltereds, currentDate) {
-    return eventsFiltereds.reduce((mayor, actual) => {
-        const eventDate = new Date(actual.date);
-        if (eventDate > new Date(currentDate) && mayor.capacity > actual.capacity) {
-            return mayor;
-        } else {
-            return actual;
-        }
-    });
 }
